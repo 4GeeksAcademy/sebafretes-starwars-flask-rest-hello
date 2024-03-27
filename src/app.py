71161cc191354a26aa77e9a4921f1e8be024bc88
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-03-27 16:24:02 +0000

    get_users and get_characters endpoints created

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, FavoriteCharacter, FavoritePlanet
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +37,40 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+
+    #First step
+    #SELECT * FROM user
+    users = User.query.all()
+
+    users_serialized = []
+    for user in users:
+        users_serialized.append(user.serialize())
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Ok",
+        "result" : users_serialized
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def get_characters():
+
+    characters = Character.query.all()
+
+    characters_serialized = []
+    for char in characters:
+        characters_serialized.append(char.serialize())
+
+    response_body = {
+        "msg" : "Ok",
+        "result" : characters_serialized
+    }
+
+    return jsonify(response_body), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,78 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
     def serialize(self):
         return {
             "id": self.id,
+            "username" : self.username,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    # def __repr__(self):
+    #     return '<User %r>' % self.username
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name
+        }
+
+class FavoriteCharacter(db.Model):
+    __tablename__ = 'favorite_character'
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    character = db.relationship(Character)
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "user_id" : self.user_id,
+            "character_id" : self.character_id
+        }
+
+
+class FavoritePlanet(db.Model):
+    __tablename__ = 'favorite_planet'
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    planet = db.relationship(Planet)
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "user_id" : self.user_id,
+            "planet_id" : self.planet_id
+        }
+
+
+    
